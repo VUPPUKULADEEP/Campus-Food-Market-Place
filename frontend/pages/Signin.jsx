e1ce047effbe63d659cef78d94140068b5b4a3bc
hemanth <hemanthkottakota07@gmail.com>
2025-12-18 19:05:43 +0530

    implemented signin page

--- frontend/pages/Signin.jsx
+++ frontend/pages/Signin.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import './Signin.css'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Signin = () => {
+
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Signin</h1></div>
+    <>
+  
+  <div className="main">
+ <div className="container">
+    <div className="inputText">email</div>
+    <input type="text" name="email" className='input' /><br />
+    <div className="inputText">password</div>
+    <input type="text" name="password" className='input' /><br />
+
+
+    <button type="button" className='loginBtn'  >login</button>
+
+    <div className="reg" onClick={()=>{navigate('/Signup')}}>regester here</div>
+  </div>
+  </div>
+  </>
   )
 }
 
