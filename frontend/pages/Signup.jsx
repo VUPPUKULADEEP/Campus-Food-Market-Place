32b6e3e9190a840f74622378dd3af0bef8387121
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2025-12-20 18:51:22 +0530

    used required

--- frontend/pages/Signup.jsx
+++ frontend/pages/Signup.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react"
+
 import {useForm} from 'react-hook-form'
 
+
+
+
 const Signup = () => {
-  const {register, handleSubmit} = useForm()
-  return (
-    <form onSubmit={handleSubmit((register)=>{ console.log(JSON.stringify(register))})}>
-      <input type="text" {...register('firstname')} />
-      <input type="text" {...register('lastname')} />
+
+
+  
+  const {register, watch, formState:{errors}, handleSubmit} = useForm()
+  
+  
+  return(
+    <form onSubmit={handleSubmit((data)=>{ console.log(data)})}>
+      <label htmlFor="firstname">Enter firstname</label>
+      
+      <input type="text" {...register('firstname', {required :'this is required'})} id="firstname" />
+      {errors.firstname && <p>{errors.firstname.message }</p>}
+      <label htmlFor="lastname">Enter Lastname</label>
+      <input type="text" {...register('lastname')} id="lastname" />
+      <label htmlFor="email"></label>
+      <input type="email" {...register('email')} id="email" />
       <input type='submit' />
     </form>
   )
