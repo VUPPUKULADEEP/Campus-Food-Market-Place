6749816b61f17ee4882b619126c1c3e53027ad90
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2025-12-21 06:32:02 +0530

    added fields in signup form

--- frontend/pages/Signup.jsx
+++ frontend/pages/Signup.jsx
@@ -5,21 +5,17 @@ import {TextField,Button} from '@mui/material'
 
 
 const Signup = () => {
+  
+  const {register, formState:{errors}, handleSubmit} = useForm()
 
-
-  
-  const {register, watch, formState:{errors}, handleSubmit} = useForm()
-  
-  
   return(
     <form onSubmit={handleSubmit((data)=>{ console.log(data)})}>
-      
-       
-      
-      <TextField id="outlined-basic" label="Outlined" variant="standard" type="text" {...register('firstname', {required :'this is required'})}  />
+      <TextField id="outlined-basic" label="firstname" variant="standard" type="text" {...register('firstname', {required :'this is required'})}  />
       {errors.firstname && <p>{errors.firstname.message }</p>}
-       <TextField id="outlined-basic" label="Outlined" variant="standard"  type="text" {...register('lastname')}/>
-      <TextField id="outlined-basic" label="Outlined" variant="standard"  type="email" {...register('email')}/>
+       <TextField id="outlined-basic" label="lastname" variant="standard"  type="text" {...register('lastname')}/>
+      <TextField id="outlined-basic" label="email" variant="standard"  type="email" {...register('email')}/>
+      <TextField id="outlined-basic" label="password" variant="standard"  type="password" {...register('email')}/>
+      <TextField id="outlined-basic" label="confirm password" variant="standard"  type="password" {...register('email')}/>
       <Button variant="contained" type='submit'>submit</Button>
     </form>
   )
