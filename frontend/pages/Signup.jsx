5cb93797fa46d61e38063ae03ee3c222e1727af1
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2025-12-21 08:04:18 +0530

    added manual css

--- frontend/pages/Signup.jsx
+++ frontend/pages/Signup.jsx
@@ -9,15 +9,15 @@ const Signup = () => {
   const { register, formState: { errors }, handleSubmit } = useForm()
 
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <form className='w-3/12 min-h-[35%] p-6 gap-1.5 border-2 border-black border-solid flex flex-col' onSubmit={handleSubmit((data) => { console.log(data) })}>
+    <div className='wrapper'>
+      <form className='form' onSubmit={handleSubmit((data) => { console.log(data) })}>
         <TextField id="outlined-basic" label="firstname" variant="standard" type="text" {...register('firstname', { required: 'this is required' })} />
         {errors.firstname && <p>{errors.firstname.message}</p>}
         <TextField id="outlined-basic" label="lastname" variant="standard" type="text" {...register('lastname')} />
         <TextField id="outlined-basic" label="email" variant="standard" type="email" {...register('email')} />
-        <TextField id="outlined-basic" label="password" variant="standard" type="password" {...register('email')} />
-        <TextField id="outlined-basic" label="confirm password" variant="standard" type="password" {...register('email')} />
-        <Button variant="contained" type='submit'>submit</Button>
+        <TextField id="outlined-basic" label="password" variant="standard" type="password" {...register('password')} />
+        <TextField id="outlined-basic" label="confirm password" variant="standard" type="password" {...register('cnf_password')} />
+        <Button className='submit' variant="contained" type='submit'>submit</Button>
       </form>
     </div>
   )
