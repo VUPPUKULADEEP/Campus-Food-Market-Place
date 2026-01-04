25ab1cde5c7d7165b18c28074a62bd8c1ee62aa4
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-01-04 06:41:42 +0530

    added home page

--- frontend/pages/Signup.jsx
+++ frontend/pages/Signup.jsx
@@ -4,46 +4,47 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Signup = () => {
 
-  const { register, formState: { errors }, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit } = useForm()
 
-  return (<>
-  <div id="container" className='d-flex justify-content-center align-items-center' style={{height:'100vh', minWidth:'50%'}}>
-        <form id="main-container" className="d-flex flex-column justify-content-start align-items-center" style={{width:'25%'}} onSubmit={handleSubmit((register)=>{ console.log(JSON.stringify(register))})}>
-            <h3>Register here </h3>
-            <div className="form-floating mb-3 col-12">
-                <input type="text" className="form-control" id="firstname" placeholder="name@example.com" {...register('firstname', { required: 'first name is required' })} />
-                <label htmlFor="firstname">firstname</label>
-                {errors.firstname && <p class='error'>{errors.firstname.message}</p>}
+    return (
+        <>
+            <div id="container" className='d-flex justify-content-center align-items-center' style={{ height: '100vh', minWidth: '50%' }}>
+                <form id="main-container" className="d-flex flex-column justify-content-start align-items-center" style={{ width: '25%' }} onSubmit={handleSubmit((register) => { console.log(JSON.stringify(register)) })}>
+                    <h3>Register here </h3>
+                    <div className="form-floating mb-3 col-12">
+                        <input type="text" className="form-control" id="firstname" placeholder="name@example.com" {...register('firstname', { required: 'first name is required' })} />
+                        <label htmlFor="firstname">firstname</label>
+                        {errors.firstname && <p class='error'>{errors.firstname.message}</p>}
+                    </div>
+                    <div className="form-floating mb-3 col-12">
+                        <input type="text" className="form-control" id="lastname" placeholder="name@example.com" {...register('lastname')} />
+                        <label htmlFor="lastname">lastname</label>
+                        {errors.lastname && <p class='error'>{errors.lastname.message}</p>}
+                    </div>
+                    <div className="form-floating mb-3 col-12">
+                        <input type="email" className="form-control" id="email" placeholder="name@example.com" {...register('email')} />
+                        <label htmlFor="email">email</label>
+                        {errors.email && <p class='error'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-floating mb-3 col-12">
+                        <input type="password" className="form-control" id="password" placeholder="xxxxxxxxxx" {...register('password')} />
+                        <label htmlFor="password">password</label>
+                        {errors.password && <p class='error'>{errors.password.message}</p>}
+                    </div>
+                    <div className="form-floating mb-3 col-12">
+                        <input type="password" className="form-control" id="cnf-password" placeholder="xxxxxxxxxx" {...register('cnfpassword')} />
+                        <label htmlFor="cnf-password">confirm password</label>
+                        {errors.cnfpassword && <p class='error'>{errors.cnfpassword.message}</p>}
+                    </div>
+                    <div className="d-grid gap-2 col-12 mx-auto">
+                        <button className="btn btn-primary" type="submit" >submit </button>
+
+                    </div>
+
+                </form>
             </div>
-            <div className="form-floating mb-3 col-12">
-                <input type="text" className="form-control" id="lastname" placeholder="name@example.com" {...register('lastname')} />
-                <label htmlFor="lastname">lastname</label>
-                {errors.lastname && <p class='error'>{errors.lastname.message}</p>}
-            </div>
-            <div className="form-floating mb-3 col-12">
-                <input type="email" className="form-control" id="email" placeholder="name@example.com" {...register('email')} />
-                <label htmlFor="email">email</label>
-                {errors.email && <p class='error'>{errors.email.message}</p>}
-        </div>
-            <div className="form-floating mb-3 col-12">
-                <input type="password" className="form-control" id="password" placeholder="xxxxxxxxxx" {...register('password')}/>
-                <label htmlFor="password">password</label>
-                {errors.password && <p class='error'>{errors.password.message}</p>}
-            </div>
-            <div className="form-floating mb-3 col-12">
-                <input type="password" className="form-control" id="cnf-password" placeholder="xxxxxxxxxx" {...register('cnfpassword')}/>
-                <label htmlFor="cnf-password">confirm password</label>
-                {errors.cnfpassword && <p class='error'>{errors.cnfpassword.message}</p>}
-            </div>
-            <div className="d-grid gap-2 col-12 mx-auto">
-                <button className="btn btn-primary" type="submit" >submit </button>
-                
-            </div>
-        
-        </form>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Signup
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Signin from "../pages/Signin"
 import Signup from "../pages/Signup"
-
+import Home from "../pages/Home"
 
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>
       </Routes>
