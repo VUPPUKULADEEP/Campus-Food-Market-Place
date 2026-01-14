941901b857e4f53e897c4d205bc349c56ccf9b88
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-01-14 09:18:40 +0530

    responsive layout designed

---
+++ frontend/components/AppBar.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './appbar.css'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const AppBar = () => {
+  
+
+  return (<>
+
+
+    <div className='bg-custom container-fluid d-flex flex-row justify-content-between p-3 text-white mx-auto'>
+      <div className='mobile'>
+      <IconButton
+
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={() => {console.log('clicked')}}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+        <Menu>
+          <MenuItem>
+          <a href='' className='text-white nav-item'>home</a>
+          </MenuItem>
+          <MenuItem>
+          <a href="" className='text-white nav-item'>products</a>
+          </MenuItem>
+          <MenuItem>
+          <a href="" className='text-white nav-item'>search</a>
+          </MenuItem>
+        </Menu>
+        </div>
+
+        <div className='d-flex align-items-center gap-2' onClick={()=>{console.log('home')}}>
+            <HomeIcon />
+            <h3 className='mb-0'>Ecommerce</h3>
+        </div>
+
+        
+        <div className='desktop container-fluid d-flex flex-row justify-content-center gap-3 text-white'>
+            <a href='' className='text-white nav-item'>home</a>
+            <a href="" className='text-white nav-item'>products</a>
+            <a href="" className='text-white nav-item'>search</a>
+
+        </div>
+        
+        <div className=' d-flex flex-row gap-4'>
+            <ShoppingCartIcon/>
+            <AccountCircleIcon/>
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default AppBar
--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ResponsiveAppBar from '../components/ResponsiveAppBar'
-
+import AppBar from '../components/AppBar'
 
 const Home = () => {
   return (
     <>
+    <AppBar/>
     <ResponsiveAppBar />
     </>
   )
