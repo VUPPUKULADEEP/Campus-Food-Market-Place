de69cad219965f2e8d1e9fa83e240233e172d103
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-01-23 05:34:14 +0530

    added card for product in home page

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import ResponsiveAppBar from '../components/ResponsiveAppBar'
 import AppBar from '../components/AppBar'
+import './home.css'
 
 const Home = () => {
   return (
     <>
     <AppBar/>
     <ResponsiveAppBar />
+    <div className="card">
+  <img src="../src/assets/sample.jpg" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     </>
   )
 }
