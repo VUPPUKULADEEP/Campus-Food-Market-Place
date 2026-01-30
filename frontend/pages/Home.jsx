ff62242c2e31f7af9e6cc0226a37f0db1ecbdff6
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-01-30 06:53:53 +0530

    added the bg-image

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import ResponsiveAppBar from '../components/ResponsiveAppBar'
 import AppBar from '../components/AppBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './home.css'
+import Card from './Card'
 
 const Home = () => {
   return (
     <>
     <AppBar/>
     <ResponsiveAppBar />
-    <div className="card">
-  <img src="../src/assets/sample.jpg" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    
+
+    <img src="../src/assets/sample2.webp" alt="not found"  className="img-fluid object-fit-cover w-100 mh-10"/>
+    
+    <Card/>
+
     </>
   )
 }
