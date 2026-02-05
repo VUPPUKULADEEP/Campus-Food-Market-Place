316b26759c720a00a0ab0bbefd0d6a579623dee8
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-02-05 20:06:06 +0530

    adding styles

--- frontend/pages/Profile.jsx
+++ frontend/pages/Profile.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import AppBar from '../components/AppBar'
+import './profile.css'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <>
+    <AppBar/>
+    <div className='container'>
+      <div className='details'>
+        <p>hello</p>
+        <h3>kuladeep v</h3>
+
+      </div>
+      <div className='menu'>
+          <a href="#"> my orders</a>
+          <a href="#"> my cart</a>
+          <a href="#"> logout</a>
+      </div>
+      <div className='editable'>
+
+      </div>
+    </div>
+    </>
   )
 }
 
