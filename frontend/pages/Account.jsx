96208f0f34eda4106c1040fa3aa7b25ba5b577fb
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-02-10 19:31:39 +0530

    added account page

--- frontend/pages/Account.jsx
+++ frontend/pages/Account.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-
+import './account.css'
 const Account = () => {
   return (<>
-  <div>
-    <div>Account</div>
-    <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis adipisci dolorum sed, cumque maxime necessitatibus sit dolor quam beatae facere laudantium, consequatur repudiandae magni obcaecati similique reiciendis suscipit voluptatibus. Iusto!
-    </p>
+  <div className='personal-info'>
+    <h4>Personal Information <a href="#">Edit</a></h4>
+    <input type="text" value={'xxxxxxxxxx'}/>
+    <input type="text"  value={'xxxxx'}/>
+    <h4>Email address <a href="#">Edit</a></h4>
+    <input type="email" value={'xxxxxx@gmail.com'} />
+    <h4>Mobile number <a href="#">Edit</a></h4>
+    <input type="number" value={'2314781902347'} />
+    <h4>address <a href="#">Edit</a></h4>
+    <textarea>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</textarea>
   </div>
-    
+    <div >
+      <p id='deactivate'>deactivate account</p>
+      <p id='delete'>delete account</p>
+    </div>
     </>
   )
 }
