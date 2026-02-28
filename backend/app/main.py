9a79bc122e1df0ca638fb7365282100acc8aa955
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-02-28 15:37:49 +0530

    tested database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from .database import get_db,init_db
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from .models import Users
 
-app = FastAPI()
+
+app = FastAPI(title='E-commerce')
+
+init_db()
 
 @app.get('/')
 def sample():
     return {'message' : 'hello world'}
+
+@app.post('/user')
+def user(email: str, db: Session = Depends(get_db)):
+    user = Users(email = email)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+@app.get('/user')
+def user(db: Session = Depends(get_db)):
+    users = db.query(Users).all()
+    return [{
+        'userid' : user.user_id,
+        'email' : user.email
+     } for user in users]
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,14 @@
 #here need to create the tables which are present in the database using sqlalchemy(orm mapper)
+
+from sqlalchemy import Column, Integer, String
+from .database import base
+
+class Users(base):
+    __tablename__ = 'Users'
+
+    user_id = Column(Integer, primary_key=True,index=True )
+    email = Column(String, unique = True, nullable=False)
+
+
+
+
