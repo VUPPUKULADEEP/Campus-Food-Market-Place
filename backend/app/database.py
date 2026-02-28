59861147ea258cb1ac7efbe0620169377c68f1b7
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-02-28 15:18:06 +0530

    implement db connection code

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,1 +1,25 @@
 # here give the database connection and get_db function
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+url = 'sqlite:///./test.db'
+engine = create_engine(url)
+
+base = declarative_base()
+
+session = sessionmaker(autoflush=False, autocommit=False, bind = engine)
+
+def init_db():
+    base.metadata.create_all(bind = engine)
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
