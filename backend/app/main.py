5bca72843c25e4fe9d81d1f9ae21959684c36d76
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-03-01 08:03:26 +0530

    added password validataion, exception management in registration form

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from .database import get_db,init_db
 from fastapi import Depends
 from sqlalchemy.orm import Session
@@ -7,6 +8,14 @@ from .schemas import UserCreate,UserResponse
 
 
 app = FastAPI(title='E-commerce')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 init_db()
 
@@ -16,11 +25,15 @@ def sample():
 
 @app.post('/user', response_model=UserResponse)
 def create_user( user:UserCreate,db: Session = Depends(get_db)):
-    user = Users(**user.dict())
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+    try:
+        user = Users(**user.dict())
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+    except Exception as e:
+        raise HTTPException(status_code=400,detail="user already exists")
+    else:
+        return user
 
 @app.get('/user', response_model=list[UserResponse])
 def get_user(db: Session = Depends(get_db)):
--- frontend/pages/Signup.jsx
+++ frontend/pages/Signup.jsx
@@ -1,35 +1,56 @@
 
 import { useForm } from 'react-hook-form'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 
 const Signup = () => {
 
-    const { register, formState: { errors }, handleSubmit } = useForm()
-    const call = () => {
+    const { register, formState: { errors }, handleSubmit, watch } = useForm()
+    const password = watch("password")
+    const apiurl = import.meta.env.VITE_BACKEND_URL;
+    console.log(import.meta.env.VITE_BACKEND_URL)
+    const onSubmit = async (data) => {
+        const {cnfpassword, ...userData} = data
+        console.log(userData)
+        let response;
+        try{
+            response = await axios.post(`${apiurl}/user`,
+            userData
+        );
+        console.log( response.data.detail)
+        }
+        catch(error){
+            
+            console.log(error.response.data.detail)
+            if (error.response.data.detail){
+                alert(error.response.data.detail)
+            }
+        }
+        
 
     }
     return (
         <>
             <div id="container" className='d-flex justify-content-center align-items-center' style={{ height: '100vh', minWidth: '50%' }}>
-                <form id="main-container" className="d-flex flex-column justify-content-start align-items-center" style={{ width: '25%' }} onSubmit={handleSubmit((register) => { console.log(JSON.stringify(register)) })}>
+                <form id="main-container" className="d-flex flex-column justify-content-start align-items-center" style={{ width: '25%' }} onSubmit={handleSubmit(onSubmit)}>
                     <h3>Register here </h3>
                     <div className="form-floating mb-3 col-12">
-                        <input type="text" className="form-control" id="firstname" placeholder="name@example.com"
-                            {...register('firstname',
+                        <input type="text" className="form-control" id="first_name" placeholder="name@example.com"
+                            {...register('first_name',
                                 {
                                     required: 'first name is required',
                                     minLength: {
                                         value: 3, message: 'minimum 3 characters'
                                     }
                                 })} />
-                        <label htmlFor="firstname">firstname</label>
-                        {errors.firstname && <p class='error'>{errors.firstname.message}</p>}
+                        <label htmlFor="first_name">firstname</label>
+                        {errors.firstname && <p class='error'>{errors.first_name.message}</p>}
                     </div>
                     <div className="form-floating mb-3 col-12">
-                        <input type="text" className="form-control" id="lastname" placeholder="name@example.com"
-                            {...register('lastname')} />
+                        <input type="text" className="form-control" id="last_name" placeholder="name@example.com"
+                            {...register('last_name')} />
                         <label htmlFor="lastname">lastname</label>
-                        {errors.lastname && <p class='error'>{errors.lastname.message}</p>}
+                        {errors.lastname && <p class='error'>{errors.last_name.message}</p>}
                     </div>
                     <div className="form-floating mb-3 col-12">
                         <input type="email" className="form-control" id="email" placeholder="name@example.com"
@@ -51,12 +72,14 @@ const Signup = () => {
                         {errors.password && <p class='error'>{errors.password.message}</p>}
                     </div>
                     <div className="form-floating mb-3 col-12">
-                        <input type="password" className="form-control" id="cnf-password" placeholder="xxxxxxxxxx" {...register('cnfpassword')} />
+                        <input type="password" className="form-control" id="cnf-password" placeholder="xxxxxxxxxx" {...register('cnfpassword',{
+                            validate: value => value === password || "password do not match"
+                        })} />
                         <label htmlFor="cnf-password">confirm password</label>
                         {errors.cnfpassword && <p className='error'>{errors.cnfpassword.message}</p>}
                     </div>
                     <div className="d-grid gap-2 col-12 mx-auto">
-                        <button className="btn btn-primary" type="submit" onClick={call}>submit </button>
+                        <button className="btn btn-primary" type="submit" >submit </button>
 
                     </div>
 
