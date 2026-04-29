67a4c409f2e28e3282cafbabcb339b203f2fb803
VUPPUKULADEEP <vuppukuladeep@gmail.com>
2026-04-29 13:12:43 +0530

    fetched user data and shown in profile section

--- frontend/pages/Account.jsx
+++ frontend/pages/Account.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './account.css'
 import axios from 'axios'
 
 
 const Account = () => {
-  window.onload = () =>{
-    const data = axios.get()
-  }
+    const [data, setData] = useState(null);
+    
+  useEffect(() =>{
+    const fetchdata = async () => {
+    const apiurl = import.meta.env.VITE_BACKEND_URL;
+    let response;
+    try {
+      response = await axios.get(`${apiurl}/users/by/${localStorage.getItem('user_id')}`);
+      console.log(response.data)
+      setData(response.data)
+    }
+    catch (error) {
+      alert('fail to fetch');
+      console.log(error)
+    }
+    }
+    fetchdata();
+  }, [])
+  
+
   return (<>
   <div className='personal-info'>
     <h4>Personal Information <a href="#">Edit</a></h4>
-    <input type="text" value={'xxxxxxxxxx'}/>
+    <input type="text" value={data?.first_name || 'xxxxxxxxxx'} readOnly />
     <h4>Email address <a href="#">Edit</a></h4>
-    <input type="email" value={'xxxxxx@gmail.com'} />
+    <input type="email" value={data?.email || 'xxxxxx@email.com'} readOnly />
     <h4>Mobile number <a href="#">Edit</a></h4>
-    <input type="number" value={'2314781902347'} />
+    <input type="number" value={data?.mobile_no || 'xxxxxxxxxx'} readOnly />
     <h4>Registration No: <a href="#">Edit</a></h4> 
-    <input type="text"  value={'xxxxxxxxxx'}/>
+    <input type="text"  value={data?.reg_no || 'xxxxxxxxxx'} readOnly />
   </div>
     <div >
       <p id='deactivate'>deactivate account</p>
